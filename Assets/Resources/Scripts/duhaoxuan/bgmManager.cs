5b9873ca6b06c3a6f0a644821df4bd48e56c54eb
kaka33xuan <kakadhx@outlook.com>
2024-07-07 15:44:52 +0800

    音效

---
+++ Assets/Resources/Scripts/duhaoxuan/bgmManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bgmManager : MonoBehaviour
+{
+    public static bgmManager instance;
+    public AudioSource audioSource;
+
+    public AudioClip startBgm;
+    public AudioClip endBgm;
+    public AudioClip inBgm;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        PlaystartBgm();
+    }
+
+    // Update is called once per frame
+    public void PlaystartBgm()
+    {
+        audioSource.clip = startBgm;
+        audioSource.Play();
+
+    }
+
+    public void PlayendBgm()
+    {
+        audioSource.clip = endBgm;
+        audioSource.Play();
+    }
+
+    public void PlayinBgm()
+    {
+        audioSource.clip = inBgm;
+        audioSource.Play();
+    }
+}
